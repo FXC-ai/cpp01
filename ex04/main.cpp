155e5f2f1e3f6b1d4f8c6c84e1acdef885bd86f4
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-06-08 13:51:11 +0200

    ex04 OK

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,41 +2,63 @@
 #include <fstream>
 #include <sstream>
 
-std::string readFileIntoString(const std::string& path) {
-    std::ifstream input_file(path);
-    if (!input_file.is_open()) 
+std::string readFileIntoString(const std::string& path) 
+{
+    std::ifstream in_file(path);
+    if (!in_file.is_open()) 
 	{
-        std::cerr << "Could not open the file - '"
-             << path << "'" << std::endl;
+        std::cerr << "Could not open the file - '" << path << "'" << std::endl;
         exit(EXIT_FAILURE);
     }
-    return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    return std::string((std::istreambuf_iterator<char>(in_file)), std::istreambuf_iterator<char>());
 }
 
-int main()
+void        readStringIntoFile(const std::string& path, std::string text)
 {
-    std::string filename("input.txt");
-    std::string file_contents;
+    std::string filename_r(path);
+
+    std::ofstream out_file(filename_r);
+    if (!out_file.is_open()) 
+	{
+        std::cerr << "Could not open the file - '" << path << "'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    out_file << text;
+    out_file.close();
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc != 4)
+    {
+        std::cerr << "Invalid number of arguments." << std::endl;
+        return 0;
+    }
+
+    const std::string   filename(argv[1]);
+    const std::string   s1(argv[2]);
+    const std::string   s2(argv[3]);
+    int                 size_s1(s1.length());
+    std::string         file_contents;
+	std::size_t         found(0);
+	std::size_t         i(0);
+    const std::string   filename_r(filename + ".replace");
 
     file_contents = readFileIntoString(filename);
 
-	std::size_t found(0);
-	std::size_t i(0);
-
 	while (found != std::string::npos)
 	{
-		found = file_contents.find("Rieux", i);
-		std::cout << found << std::endl;
-		//file_contents.erase(0, 5);
-		i = found + 1;
+		found = file_contents.find(s1, i);
+        if (found != std::string::npos)
+        {
+		    file_contents.erase(found, size_s1);
+            file_contents.insert(found, s2);
+		    i = found + 1;
+        }
 	}
 
-
-    //std::cout << found << std::endl;
-
-
-
-    std::cout << file_contents << std::endl;
-
+    readStringIntoFile(filename_r, file_contents);
+ 
     exit(EXIT_SUCCESS);
 }
